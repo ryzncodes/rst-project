157e3fc05322e053c3149c3b2522cb69f8424ee3
Faiz <faizkasman97@gmail.com>
2022-05-15 20:04:55 +0800

    added wallet connection function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
+
 
 // Constants
 const TWITTER_HANDLE = 'ryzncodes';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  /* checkIfWalletIsConnected is to check if the window is connected
+  to the Phantom wallet. The wallet are used for solana web3 app.  */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+      
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+        }
+      } else {
+        alert('Solana wallet not found! Install Phantom Wallet to be connected.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+/**When webpage first loads, useEffect will run once to run the
+ * checkIf.. to see if wallet connected or not.
+ */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener ('load', onLoad);
+  }, []);  
+
   return (
     <div className="App">
       <div className="container">
